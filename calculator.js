d0675905474005ec8f551a8ca8d1b157cf9c55ae
LaloCode <lalo_pichu22@hotmail.com>
2021-12-02 22:00:04 -0600

    Add dot behavior

--- calculator.js
+++ calculator.js
@@ -3,12 +3,14 @@ let mainNum = '0';
 const clearBtn = document.getElementById('clear');
 const deleteBtn = document.getElementById('delete');
 const mainResult = document.getElementById('MainResult');
+const dotBtn = document.getElementById('dot');
 const buttons = document.getElementsByClassName('Number');
+
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', (e) => addToMainNum(e.target.textContent));
 }
-
 document.addEventListener('keydown', checkKeyPress);
+dotBtn.addEventListener('click', addDot);
 
 function addToMainNum(number) {
   if (mainNum === '0') {
@@ -19,9 +21,23 @@ function addToMainNum(number) {
   mainResult.textContent = mainNum;
 }
 
+function addDot() {
+  for (let i = 0; i < mainNum.length; i++) {
+    if (mainNum[i] === '.') {
+      return;
+    }
+  }
+
+  mainNum += '.';
+}
+
 function checkKeyPress(e) {
   console.log(e.key);
+
   if (/^[0-9]$/i.test(e.key)) {
     addToMainNum(e.key);
+  }
+  if (e.key === '.') {
+    addDot();
   }
 }
